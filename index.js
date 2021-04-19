eeaac580fab8f89aac4082f83995999e2d3a84d8
Hpollard003 <hpollard003@gmail.com>
2021-04-19 18:05:37 -0400

    commit tree a8c46a11c484ab52a9b820a7e898afa78a6ae26a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const users = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+    })
+};
+const body = document.querySelector('body');
+function submitData(){
+return fetch('http://localhost:3000/users', users)
+.then(resp => resp.json())
+.then((data) => {
+    body.innerHTML = data.id
+})
+.catch(error => {
+    alert('Unauthorized Access');
+    console.log(error);
+    body.innerHTML = error.message
+})
+}
+
