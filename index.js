7c7463c8e1a047825e7c77837f05c1067235a0a5
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-07-15 13:00:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object){
+        const p = document.createElement("p");
+        p.innerText = object.id;
+        document.body.appendChild(p);
+        console.log(object);
+    })
+    .catch(function(error) {
+        const header = document.createElement("header");
+        header.innerHTML = error.message;
+        document.body.appendChild(header);
+        console.log(error.message);
+      });
+};
